5f1cae345de1870f5a12c06faa3410b84bbbaf0a
othelarian <le.maitre.killian@gmail.com>
2019-07-28 13:50:45 +0200

    back rust fini

--- front/src/TestIdentityEndpoint.elm
+++ front/src/TestIdentityEndpoint.elm
@@ -5,9 +5,11 @@ import Browser.Navigation as Nav
 import Html exposing (Html, button, div, iframe, p, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import Http
 import Url
 import Url.Builder as UB
 import Url.Parser as UP
+import Url.Parser exposing(s, (<?>), (</>))
 import Url.Parser.Query as UPQ
 
 import Debug
@@ -27,27 +29,70 @@ main =
 
 -- MODEL
 
+type Route
+    = Sill
+    | Lost
+    | Gate String
+    | Castle String
+
 type Phase
     = Init
 
 type alias Model =
     { url : Url.Url
+    , route : Route
+    , code : String
     , phase : Phase
     }
 
 init : () -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
 init _ url _ =
     let
-        --
-        _ = Debug.log "TODO" "parse query"
-        --
-        getCode = Nothing --UP.parse parseQueryCode url
-        --
+        getRouteParser search = UP.query (UPQ.string search)
+        getClearedUrl = {url | path = ""}
+        getRefresh =
+            case UP.parse (getRouteParser "refresh") getClearedUrl of
+                Just res -> res
+                Nothing -> Nothing
+        getCode =
+            case UP.parse (getRouteParser "code") getClearedUrl of
+                Just res -> res
+                Nothing -> Nothing
+        getLost =
+            case UP.parse (getRouteParser "lost") getClearedUrl of
+                Just res -> res
+                Nothing -> Nothing
+        getRoute =
+            case getRefresh of
+                Just token -> Castle token
+                Nothing -> case getCode of
+                    Just code -> Gate code
+                    Nothing -> case getLost of
+                        Just _ -> Lost
+                        Nothing -> Sill
     in
-    ( Model url Init
-    , case getCode of
-        Just code -> Cmd.none
-        Nothing -> Cmd.none
+    ( Model url getRoute "" Init
+    , case getRefresh of
+        Just token ->
+            --
+            -- TODO : initialisation de l'app avec le refresh token
+            --
+            Cmd.none
+            --
+        Nothing ->
+            case getCode of
+                Just code ->
+                    --
+                    -- TODO : request the refresh token
+                    --
+                    -- TODO : pour les tests, on va d'abord envoyer un code bidon vers le rust pour voir si ça fonctionne
+                    --
+                    Http.get
+                        { url = "/saveToken?token=abcde"
+                        , expect = Http.expectString SaveAnswer
+                        }
+                    --
+                Nothing -> Cmd.none
     )
 
 -- HANDLERS
@@ -65,9 +110,9 @@ createIdentityUrl =
         , UB.string "client_id" Keys.getClientId
         ]
 
-parseQueryCode : UPQ.Parser (Maybe String)
+parseQueryCode : UP.Parser (Maybe String -> Maybe String) (Maybe String)
 parseQueryCode =
-    UPQ.string "code"
+    UP.query (UPQ.string "code")
 
 -- UPDATE
 
@@ -75,17 +120,25 @@ type Msg
     = UrlChanged Url.Url
     | UrlRequested Browser.UrlRequest
     | Connection
+    | SaveAnswer (Result Http.Error String)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        UrlChanged url ->
-            --
-            --
-            (model, Cmd.none)
-            --
+        UrlChanged url -> (model, Cmd.none)
         UrlRequested urlRequested -> (model, Cmd.none)
         Connection -> (model, Nav.load createIdentityUrl)
+        SaveAnswer answer ->
+            case answer of
+                Ok rep ->
+                    let
+                        _ = Debug.log "rep" rep
+                    in
+                    --
+                    --
+                    (model, Cmd.none)
+                    --
+                Err _ -> (model, Cmd.none)
 
 -- SUB
 
@@ -99,7 +152,14 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "identity endpoint test"
     , body =
-        [ p [] [text ("status: "++"")]
+        [ p [] [text ("code: "++model.code)]
+        , (
+            case model.route of
+                Sill -> p [] [text "sending SILL"]
+                Lost -> p [] [text "We are lost..."]
+                Gate _ -> p [] [text "Front of the gate, ready to use the code"]
+                Castle _ -> p [] [text "This castle is mine!"]
+        )
         , p [] [text ("url: "++(Url.toString model.url))]
         , case model.phase of
             Init -> button [onClick Connection] [text "Connexion"]
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,57 @@
+extern crate directories;
 extern crate micro_http_server;
 extern crate web_view;
 
+use directories::ProjectDirs;
 use micro_http_server::{MicroHTTP, Client};
 use web_view::*;
-
+use std::io;
+use std::fs;
 use std::path::Path;
 use std::{thread, time};
 
+struct ConfigView<'a> {
+    url: &'a str,
+    width: i32,
+    height: i32
+}
+
 fn main() {
     // check configuration file
-    //
-    // TODO : initialisation des données de configuration, avec notamment le refresh token s'il est présent
-    //
+    match ProjectDirs::from("inc", "othelarian", "tracy") {
+        Some(proj_dirs) => {
+            let config_path = proj_dirs.config_dir();
+            match config_path.exists() {
+                true => {
+                    let pathbuf = config_path.join("tracy.conf");
+                    let config_file = pathbuf.as_path();
+                    match config_file.exists() {
+                        true => Ok(()),
+                        false => {
+                            match fs::write(config_file, "") {
+                                Ok(_) => Ok(()),
+                                Err(_) => Err(())
+                            }
+                        }
+                    }
+                }
+                false => {
+                    match fs::create_dir_all(config_path) {
+                        Ok(_) => {
+                            let pathbuf = config_path.join("tracy.conf");
+                            let config_file = pathbuf.as_path();
+                            match fs::write(config_file, "") {
+                                Ok(_) => Ok(()),
+                                Err(_) => Err(())
+                            }
+                        }
+                        Err(_) => Err(())
+                    }
+                }
+            }
+        }
+        None => Err(())
+    }.unwrap();
     // starting micro server
     let server = MicroHTTP::new("127.0.0.1:8000")
         .expect("Cound not create server.");
@@ -30,19 +70,22 @@ fn main() {
                         .expect("Could not send data to client.");
                 } else {
                     let request_copy: &str = client.request().as_ref().unwrap();
-                    let sending = match request_copy {
-                        "/" => get_statics(&mut client, "app.html"),
-                        "/theme.css" => get_statics(&mut client, "theme.css"),
-                        "/app.js" => get_statics(&mut client, "app.js"),
-                        "/keys.js" => get_statics(&mut client, "keys.js"),
-                        //
-                        "/test.html" => get_statics(&mut client, "test.html"),
-                        "/testEndpoints.js" => get_statics(&mut client, "testEndpoints.js"),
-                        //
-                        // TODO : ajouter les entrées manquantes (notamment pour le refresh token)
-                        //
-                        //
-                        _ => client.respond("404 Not Found", "No resource found".as_bytes(), &vec!())
+                    let url_split: Vec<&str> = request_copy.split('?').collect();
+                    let sending = match url_split.get(0) {
+                        Some(path) => {
+                            match path {
+                                &"/" => get_statics(&mut client, "app.html"),
+                                &"/app.html" => get_statics(&mut client, "app.html"),
+                                &"/theme.css" => get_statics(&mut client, "theme.css"),
+                                &"/app.js" => get_statics(&mut client, "app.js"),
+                                &"/keys.js" => get_statics(&mut client, "keys.js"),
+                                &"/getToken" => get_token(&mut client),
+                                &"/saveToken" => save_token(&mut client),
+                                &"/windows" => get_statics(&mut client, "windows.html"),
+                                _ => client.respond("404 Not Found", "No resource found".as_bytes(), &vec!())
+                            }
+                        }
+                        None => client.respond("400 Bad Request", "Bad request".as_bytes(), &vec!())
                     };
                     sending.expect("Could not send data to client.");
                 }
@@ -50,36 +93,37 @@ fn main() {
         }
     });
     // start the web view
+    let config_view = if cfg!(windows) {
+        println!("web view support on windows is bad enough to go play with the browser");
+        ConfigView {url: "http://localhost:8000/windows", width: 250, height: 200}
+    } else {
+        ConfigView {url: "http://localhost:8000/", width: 600, height: 800}
+    };
     web_view::builder()
         .title("Tracy")
-        .content(Content::Url("http://localhost:8000/test.html"))
-        .size(600, 800)
+        .content(Content::Url(config_view.url))
+        .size(config_view.width, config_view.height)
         .user_data(())
-        //.debug(true)
+        .debug(true)
         .invoke_handler(|_webview, _arg| Ok(()))
         .run()
         .unwrap();
 }
 
-fn get_statics(client: &mut Client, resource: &str) -> std::io::Result<usize> {
+fn get_statics(client: &mut Client, resource: &str) -> io::Result<usize> {
     let result = if cfg!(debug_assertions) {
         let fp = &format!("front/{}", resource);
         match Path::new(fp).exists() {
-            true => std::fs::read_to_string(fp),
-            false => Err(std::io::Error::new(std::io::ErrorKind::Other, "No file found"))
+            true => fs::read_to_string(fp),
+            false => Err(io::Error::new(io::ErrorKind::Other, "No file found"))
         }
     } else {
         match resource {
             "app.html" => Ok(format!("{}", include_str!("../front/app.html"))),
             "theme.css" => Ok(format!("{}", include_str!("../front/theme.css"))),
             "app.js" => Ok(format!("{}", include_str!("../front/app.js"))),
-            //"keys.js" => Ok(format!("{}", include_str!("../front/keys.js"))),
-            //
-            //
-            // TODO : ajouter les champs manquant
-            //
-            //
-            _ => Err(std::io::Error::new(std::io::ErrorKind::Other, "We never found what you asked for"))
+            "windows.html" => Ok(format!("{}", include_str!("../front/windows.html"))),
+            _ => Err(io::Error::new(io::ErrorKind::Other, "We never found what you asked for"))
         }
     };
     match result {
@@ -88,5 +132,36 @@ fn get_statics(client: &mut Client, resource: &str) -> std::io::Result<usize> {
             let msg = format!("Error on providing static resource: {}", error);
             client.respond("500 Server Error", msg.as_bytes(), &vec!())
         }
+    }
+}
+
+fn get_token(client: &mut Client) -> io::Result<usize> {
+    match ProjectDirs::from("inc", "othelarian", "tracy") {
+        Some(proj_dirs) => {
+            let config_path = proj_dirs.config_dir();
+            let pathbuf = config_path.join("tracy.conf");
+            let config_file = pathbuf.as_path();
+            let rep = match fs::read_to_string(config_file) {
+                Ok(token) => token, Err(_) => String::from("failed")
+            };
+            client.respond_ok(rep.as_bytes())
+        }
+        None => client.respond_ok("failed".as_bytes())
+    }
+}
+
+fn save_token(client: &mut Client) -> io::Result<usize> {
+    match ProjectDirs::from("inc", "othelarian", "tracy") {
+        Some(proj_dirs) => {
+            let token = &client.request().as_ref().unwrap().to_owned()[17..];
+            let config_path = proj_dirs.config_dir();
+            let pathbuf = config_path.join("tracy.conf");
+            let config_file = pathbuf.as_path();
+            let rep = match fs::write(config_file, token) {
+                Ok(_) => "saved", Err(_) => "failed"
+            };
+            client.respond_ok(rep.as_bytes())
+        }
+        None => client.respond_ok("failed".as_bytes())
     }
 }
